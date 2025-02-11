a64229b0a093fc61be6c1580b67e52a0681d1f59
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2025-02-11 08:57:08 +0700

    Half base code completed

---
+++ NewsManagementSystem/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using Repositories.PaggingItem;
+
+namespace Repositories.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //queryable
+        IQueryable<T> Entities { get; }
+
+        //void
+        T? GetById(object id);
+        void Insert(T obj);
+        void InsertRange(List<T> obj);
+        Task InsertRangeAsync(List<T> obj);
+
+        void Update(T obj);
+        void Delete(object entity);
+        void Save();
+
+        //Task
+        Task<T?> GetByIdAsync(object id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteAsync(object entity);
+        Task SaveAsync();
+
+        //another
+        T? Find(Expression<Func<T, bool>> predicate);
+        Task<PaginatedList<T>> GetPagging(IQueryable<T> query, int index, int pageSize);
+    }
+}
---
+++ NewsManagementSystem/Repositories/Interfaces/IUOW.cs
@@ -0,0 +1,13 @@
+﻿namespace Repositories.Interface
+{
+    public interface IUOW : IDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : class;
+        void Save();
+        Task SaveAsync();
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollBack();
+
+    }
+}
