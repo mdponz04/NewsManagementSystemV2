0210ddaa4ea38259a392ddcd35705d111c22e088
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2025-02-16 13:46:03 +0700

    fix: authorization

--- NewsManagementSystem/NewsManagementSystem/Controllers/HomeController.cs
+++ NewsManagementSystem/NewsManagementSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using Data.Enum;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,23 @@ namespace NewsManagementSystem.Controllers
         [Authorize(Roles = "2")]
         public IActionResult SecurePage()
         {
+            var jwtTokenFromSession = HttpContext.Session.GetString("jwt_token");
+
+            // Decode the JWT token to extract the user's name
+            string userName = string.Empty;
+            if (!string.IsNullOrEmpty(jwtTokenFromSession))
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var jsonToken = handler.ReadToken(jwtTokenFromSession) as JwtSecurityToken;
+                if (jsonToken != null)
+                {
+                    // Assuming the user's name is stored in the "name" claim
+                    userName = jsonToken.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+                }
+            }
+
+            // Pass the user name to the view
+            ViewData["UserName"] = userName;
 
             return View("SecurePage");
         }
