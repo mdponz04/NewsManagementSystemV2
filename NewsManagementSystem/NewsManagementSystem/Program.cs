8898fc4ec749aaea2c0150da28ccada9ef6c41d5
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2025-02-16 13:25:24 +0700

    fix: logout deleted jwt token

--- NewsManagementSystem/NewsManagementSystem/Program.cs
+++ NewsManagementSystem/NewsManagementSystem/Program.cs
@@ -44,6 +44,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+app.UseMiddleware<JwtTokenMiddleware>();  // JWT middleware
 
 app.UseRouting();
 
