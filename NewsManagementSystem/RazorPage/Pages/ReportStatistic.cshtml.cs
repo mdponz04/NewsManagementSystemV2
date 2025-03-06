6460374811e0a77e3a8a229c686f7a064bac7c50
Phan Pham Hoa <hoappse.171730@fpt.edu.vn>
2025-03-06 15:34:13 +0700

    feat: update filter for dashboard + change dd-mm-yyyyy

--- NewsManagementSystem/RazorPage/Pages/ReportStatistic.cshtml.cs
+++ NewsManagementSystem/RazorPage/Pages/ReportStatistic.cshtml.cs
@@ -14,15 +14,38 @@ namespace RazorPage.Pages
             _newsArticleService = newsArticleService;
         }
 
+        // Bindable properties to receive the filter dates from the query string.
+        [BindProperty(SupportsGet = true)]
+        public DateTime? FromDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? ToDate { get; set; }
+
         // This list holds the grouped report data to be passed to the view.
         public List<NewsCreationReport> NewsCreationReports { get; set; } = new List<NewsCreationReport>();
 
         public async Task OnGetAsync()
         {
-            // Retrieve all news articles using your service
+            // Retrieve all news articles using your service.
             List<GetNewsArticleDTO> newsArticles = await _newsArticleService.GetAllNewsArticle();
 
-            // Group by the date the news article was created (ignoring the time part)
+            // Apply the date filter if values are provided.
+            if (FromDate.HasValue)
+            {
+                newsArticles = newsArticles
+                    .Where(n => n.CreatedDate.HasValue &&
+                           n.CreatedDate.Value.Date >= FromDate.Value.Date)
+                    .ToList();
+            }
+            if (ToDate.HasValue)
+            {
+                newsArticles = newsArticles
+                    .Where(n => n.CreatedDate.HasValue &&
+                           n.CreatedDate.Value.Date <= ToDate.Value.Date)
+                    .ToList();
+            }
+
+            // Group by the date the news article was created (ignoring the time part).
             NewsCreationReports = newsArticles
                 .Where(n => n.CreatedDate.HasValue)
                 .GroupBy(n => n.CreatedDate.Value.Date)
@@ -35,6 +58,4 @@ namespace RazorPage.Pages
                 .ToList();
         }
     }
-
-
 }
